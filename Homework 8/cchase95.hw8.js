d97209a96a74e4105f9d03f61d631e850d316ed7
Chase_Crawford <chasecrawford95@gmail.com>
2023-10-08 19:37:28 -0500

    Created HW8 Folder. Added HW8 HTML and JS files. Added in grader with reward system

---
+++ Homework 8/cchase95.hw8.js
@@ -0,0 +1,41 @@
+
+
+function test(){
+    let input = prompt("Please enter a number");
+    document.getElementById("GR").innerHTML = input
+    switch(true){
+        case input >= 90:
+            console.log("A")
+            document.getElementById("Grade").innerHTML="Grade: " + input
+            document.getElementById("LeterGrade").innerHTML="Letter Grade: A"
+            document.getElementById("Reward").innerHTML="Reward: Candy"
+            break;    
+        case input >= 80 && input <= 89:
+            console.log("B")
+            document.getElementById("Grade").innerHTML="Grade: " + input
+            document.getElementById("LeterGrade").innerHTML="Letter Grade: B"
+            document.getElementById("Reward").innerHTML="Reward: Apple"
+            break;
+        case input >= 70 && input <=79:
+            console.log("C")
+            document.getElementById("Grade").innerHTML="Grade: " + input
+            document.getElementById("LeterGrade").innerHTML="Letter Grade: C"
+            document.getElementById("Reward").innerHTML="Reward: Mint"
+            break;
+        case input >= 60 && input <= 69:
+            console.log("D")
+            document.getElementById("Grade").innerHTML="Grade: " + input
+            document.getElementById("LeterGrade").innerHTML="Letter Grade: D"
+            document.getElementById("Reward").innerHTML="Reward: Pencil"
+            break;
+        case input < 60:
+            console.log("F")
+            document.getElementById("Grade").innerHTML="Grade: " + input
+            document.getElementById("LeterGrade").innerHTML="Letter Grade: F"
+            document.getElementById("Reward").innerHTML="Reward: Awkward conversation with parents"
+            break;
+        case input = null:
+            document.getElementById("GR").innerHTML = "Please enter a number..."
+            break;
+    }
+}
