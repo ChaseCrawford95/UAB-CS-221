ff86b4b4743cbb1f3fa707ac8ccc9f43ec6f5126
Chase_Crawford <chasecrawford95@gmail.com>
2023-10-20 15:11:34 -0500

    Sources Update

--- Homework 8/cchase95.hw8.js
+++ Homework 8/cchase95.hw8.js
@@ -49,4 +49,4 @@ function test(){
 // Sources
 // I wanted to figure out how to make a button be used to open up the prompt function. Also how to make a button work in conjucntion with a JavaScript Function. I got help from w3schools with this example: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt
 // That same example also helped me figure out how to make a JavaScript file manipulate certain tags on the HTML file
-// I had to figure out how to handle inputs that included words, letters, open blank spaces, and nothing inputted at all by the user. ChatGPT suggested I use .trim() function to handle any open white space. 
+// I had to figure out how to handle inputs that included words, letters, open blank spaces, and nothing inputted at all by the user. ChatGPT suggested I use .trim() function to handle any open white space input. 
